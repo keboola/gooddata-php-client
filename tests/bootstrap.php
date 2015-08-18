92fd4f234f1b68d5ac9acfe744585401bc06b918
Jakub Matejka <jakub@keboola.com>
2015-08-18 17:40:20 +0200

    test: circleci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,13 +6,17 @@ date_default_timezone_set('Europe/Prague');
 
 defined('GOODDATA_USERNAME')
 || define('GOODDATA_USERNAME', getenv('GOODDATA_USERNAME') ? getenv('GOODDATA_USERNAME') : 'username');
+
 defined('GOODDATA_PASSWORD')
 || define('GOODDATA_PASSWORD', getenv('GOODDATA_PASSWORD') ? getenv('GOODDATA_PASSWORD') : 'password');
+
 defined('GOODDATA_URL')
 || define('GOODDATA_URL', getenv('GOODDATA_URL') ? getenv('GOODDATA_URL') : 'https://secure.gooddata.com');
+
 defined('GOODDATA_DOMAIN')
 || define('GOODDATA_DOMAIN', getenv('GOODDATA_DOMAIN') ? getenv('GOODDATA_DOMAIN') : 'domain');
+
 defined('GOODDATA_AUTH_TOKEN')
 || define('GOODDATA_AUTH_TOKEN', getenv('GOODDATA_AUTH_TOKEN') ? getenv('GOODDATA_AUTH_TOKEN') : 'auth_token');
 
-require_once ROOT_PATH . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
