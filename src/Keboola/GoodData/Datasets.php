2fe097a136e1e68d56e208facee98df61b52adc4
Jakub Matějka <jakub@keboola.com>
2019-01-08 06:45:25 +0100

    Make etl job polling constant (fixes #5) (#6)

--- src/Keboola/GoodData/Datasets.php
+++ src/Keboola/GoodData/Datasets.php
@@ -100,7 +100,7 @@ class Datasets
     {
         $try = 1;
         do {
-            sleep(10 * $try);
+            sleep(min(pow(2, $try), rand(15, 20)));
             $taskResponse = $this->client->get($uri);
 
             if (!isset($taskResponse['wTaskStatus']['status'])) {
